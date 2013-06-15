7b591ad7b224783197f0a73f8ae65f76bfc477f6
Hao Luo <howlowck@gmail.com>
2013-06-14 23:12:47 -0500

    more performance

--- public/js/convolve.js
+++ public/js/convolve.js
@@ -1,6 +1,5 @@
 var convolve = function (oldPixels, width, convolveMatrix, divisor, offset) {
-    var //conMatrixFlatten = convolveMatrix.toString().split(','),
-        cmat0 = convolveMatrix[0][0],
+    var cmat0 = convolveMatrix[0][0],
         cmat1 = convolveMatrix[0][1],
         cmat2 = convolveMatrix[0][2],
         cmat3 = convolveMatrix[1][0],
@@ -9,48 +8,37 @@ var convolve = function (oldPixels, width, convolveMatrix, divisor, offset) {
         cmat6 = convolveMatrix[2][0],
         cmat7 = convolveMatrix[2][1],
         cmat8 = convolveMatrix[2][2],
-        //conMatrixLength = conMatrixFlatten.length,
+        result = 0,
         oldPixelsLength = oldPixels.length,
-        newpixels = [];
+        newpixels = [],
+        currentPixel,
+        topDim,
+        bottomDim;
 
     for (var i = 0; i < oldPixelsLength; i++) {
-        var currentPixel = oldPixels[i],
-            topDim = i - width * 4,
-            bottomDim = i + width * 4;
+        currentPixel = oldPixels[i];
+        topDim = i - width * 4;
+        bottomDim = i + width * 4;
 
         if ((i + 1) % 4 === 0) {
             newpixels[i] = currentPixel;
             continue;
         }
-
-        var these = [
-            oldPixels[topDim - 4] || null,
-            oldPixels[topDim]     || null,
-            oldPixels[topDim + 4] || null,
-            oldPixels[i - 4]         || null,
-            null,
-            oldPixels[i + 4]         || null,
-            oldPixels[bottomDim - 4] || null,
-            oldPixels[bottomDim]     || null,
-            oldPixels[bottomDim + 4] ||null 
-        ];
-        var result = 0;
-        // for (var j = 0; j < conMatrixLength; j++) {
-        //     result += these[j] * conMatrixFlatten[j];
-        // }
         /* Unroll loop for performance but restricts to 3x3 */
-        result += these[0] * cmat0;
-        result += these[1] * cmat1;
-        result += these[2] * cmat2;
-        result += these[3] * cmat3;
+
+        result += (oldPixels[topDim - 4] || null) * cmat0;
+        result += (oldPixels[topDim]     || null) * cmat1;
+        result += (oldPixels[topDim + 4] || null) * cmat2;
+        result += (oldPixels[i - 4] || null) * cmat3;
         result += currentPixel * cmat4;
-        result += these[5] * cmat5;
-        result += these[6] * cmat6;
-        result += these[7] * cmat7;
-        result += these[8] * cmat8;
+        result += (oldPixels[i + 4] || null) * cmat5;
+        result += (oldPixels[bottomDim - 4] || null) * cmat6;
+        result += (oldPixels[bottomDim] || null) * cmat7;
+        result += (oldPixels[bottomDim + 4] || null) * cmat8;
 
         result /= divisor;
         newpixels[i] = result + offset;
+        result = 0;
     }
     return newpixels;
 };
